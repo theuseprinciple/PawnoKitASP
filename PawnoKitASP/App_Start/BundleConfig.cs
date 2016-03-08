ba77312c0c8627732027d6b12ae0ab13c4da636d
Dmitry <Dmitry@Dmitry-PC>
2016-03-08 05:18:53 +0500

    455

--- PawnoKitASP/App_Start/BundleConfig.cs
+++ PawnoKitASP/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace PawnoKitASP
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            /*
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -27,6 +28,9 @@ namespace PawnoKitASP
                       "~/Content/site.css"//,
                       //"~/Content/bootstrap.css"
                       ));
+                      */
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                       "~/Content/site.css"));
         }
     }
 }
---
+++ PawnoKitASP/Content/IndexJs.js
@@ -0,0 +1,23 @@
+﻿var CurrentModule = {
+    Install: function () {
+        $("#pk-quote").css('color', "green");
+        alert("script iss loaded");
+    },
+    Remove: function () { 
+        alert("Element was removed");
+        $("#module").unbind("destroyed");
+    },
+};
+
+
+(function () {
+
+    CurrentModule.Install();
+
+    $('#module').bind('destroyed', function () {
+        CurrentModule.Remove();
+        delete CurrentModule;
+    });
+})();
+
+
