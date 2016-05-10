b1721780db2f700201043ce5f41d35d58ade3ef2
Dima Diman <bellju@yandex.ru>
2016-05-10 11:09:17 +0500

    99

--- PawnoKitASP/Controllers/RuController.cs
+++ PawnoKitASP/Controllers/RuController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PawnoKitASP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,7 +14,7 @@ namespace PawnoKitASP.Controllers
         // ужас, но на первое время сойдет
         // GET: En
         public ActionResult Index(string hash) {
-            if(hash != null)
+            if (hash != null)
             {
                 try
                 {
